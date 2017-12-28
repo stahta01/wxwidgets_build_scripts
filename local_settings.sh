70715dde36ca411617858e44a2476a699d5d53dc
Tim S <stahta01@users.sourceforge.net>
2017-12-28 04:12:30 -0500

    Add local_settings.sh

---
+++ local_settings.sh
@@ -0,0 +1,12 @@
+# local user settings
+
+# COMPILER_640TDM_BASE can not have a colon ":" in it.
+COMPILER_640TDM_BASE="/C/GreenApps32/mingw32-TDM-gcc-6.4.0-2"
+
+# MSYS2_COMPILER_BASE can not have a colon ":" in it.
+MSYS2_COMPILER_BASE="/C/Apps32/MSys2/mingw32"
+MSYS2_COMPILER_VERSION="720MSYS2"
+
+WXWIDGETS_3_0_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets_3_0_branch"
+WXWIDGETS_3_0_TEST_BRANCH="testing3.0"
+WXWIDGETS_3_0_LFS_BRANCH="wxwidgets3.0.3_git-lfs"
