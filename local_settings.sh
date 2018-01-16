b58844769ebfc8a5532b1a783b6e069e85cf0ab7
Tim S <stahta01@users.sourceforge.net>
2018-01-15 20:01:48 -0500

    Edit: local_settings.sh

--- local_settings.sh
+++ local_settings.sh
@@ -1,17 +1,24 @@
 # local user settings
 
-# COMPILER_640TDM_BASE and COMPILER_510TDM_BASE 
+# COMPILER_640TDM_BASE, COMPILER_510TDM_BASE, and MINGW_ORG_COMPILER_BASE
 #    can not have a colon ":" in it.
 COMPILER_640TDM_BASE="/C/GreenApps32/mingw32-TDM-gcc-6.4.0-2"
 COMPILER_510TDM_BASE="/C/GreenApps32/MinGW_cb17.12"
+MINGW_ORG_COMPILER_BASE="/C/Apps32/mingw.org"
+MINGW_ORG_COMPILER_VERSION="630M32"
 
 
-# MSYS2_COMPILER_BASE and MSYS2_COMPILER_X64_BASE 
+# MSYS2_BASE and MSYS2_X64_BASE 
 #    can not have a colon ":" in it.
-MSYS2_COMPILER_BASE="/C/Apps32/MSys2/mingw32"
-MSYS2_COMPILER_X64_BASE="/C/Apps64/MSys2/mingw64"
-MSYS2_COMPILER_VERSION="720MSYS2"
+MSYS2_BASE="/C/Apps32/MSys2"
+MSYS2_X64_BASE="/C/Apps64/MSys2"
+MSYS2_COMPILER_BASE="$MSYS2_BASE/mingw32"
+MSYS2_COMPILER_X64_BASE="$MSYS2_X64_BASE/mingw64"
+MSYS2_COMPILER_VERSION="730MSYS2"
 
 WXWIDGETS_3_0_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets_3_0_branch"
 WXWIDGETS_3_0_TEST_BRANCH="testing3.0"
 WXWIDGETS_3_0_LFS_BRANCH="wxwidgets3.0.3_git-lfs"
+
+WXWIDGETS_2_8_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets_2_8_branch"
+WXWIDGETS_2_8_LFS_BRANCH="wxwidgets2.8.12_git-lfs"
