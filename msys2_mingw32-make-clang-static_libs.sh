61576eac5f53ab43534576de8b5950242abeb43e
Tim S <stahta01@users.sourceforge.net>
2018-02-16 20:29:59 -0500

    * Add: msys2_mingw32-make-clang-static_libs.sh

---
+++ msys2_mingw32-make-clang-static_libs.sh
@@ -0,0 +1,34 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+# import local settings file
+source local_settings.sh
+
+# Change to git folder
+cd $WXWIDGETS_REPO || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout clang/makefile.gcc || exit 2
+# git status -uno
+
+
+export PATH=$MSYS2_COMPILER_BASE/bin:$PATH
+
+# echo $PATH
+
+# echo $MSYS2_COMPILER_VERSION
+
+cd build/msw || exit 3
+
+mingw32-make -f makefile.gcc \
+  CC=clang CXX=clang++ \
+  COMPILER_VERSION=$MSYS2_CLANG_VERSION \
+  MONOLITHIC=0 SHARED=0 UNICODE=1 BUILD=debug
+
+mingw32-make -f makefile.gcc \
+  CC=clang CXX=clang++ \
+  COMPILER_VERSION=$MSYS2_CLANG_VERSION \
+  MONOLITHIC=0 SHARED=0 UNICODE=1 BUILD=release
+
+# echo "Finished"
