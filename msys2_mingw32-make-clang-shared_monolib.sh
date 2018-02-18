1e6da47601ccfa34a7677fe60c4b737dbf2f9ada
Tim S <stahta01@users.sourceforge.net>
2018-02-18 00:01:38 -0500

    * Add: msys2_mingw32-make-clang-shared_monolib.sh

---
+++ msys2_mingw32-make-clang-shared_monolib.sh
@@ -0,0 +1,38 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+# import local settings file
+source local_settings.sh
+
+# Change to git folder
+cd $WXWIDGETS_REPO || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout clang/makefile.gcc || exit 2
+# git status -uno
+
+
+export PATH=$MSYS2_COMPILER_BASE/bin:$PATH
+
+# echo $PATH
+
+# echo $MSYS2_COMPILER_VERSION
+
+cd build/msw || exit 3
+
+mingw32-make -f makefile.gcc \
+  CC=clang CXX=clang++ \
+  CFG=mono \
+  COMPILER_VERSION=$MSYS2_CLANG_VERSION \
+  LDFLAGS=-Wl,--allow-multiple-definition \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=debug
+
+mingw32-make -f makefile.gcc \
+  CC=clang CXX=clang++ \
+  CFG=mono \
+  COMPILER_VERSION=$MSYS2_CLANG_VERSION \
+  LDFLAGS=-Wl,--allow-multiple-definition \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release
+
+# echo "Finished"
