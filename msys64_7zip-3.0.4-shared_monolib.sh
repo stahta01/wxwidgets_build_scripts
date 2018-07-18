9079ed47f732a385d98c852f32eeda5407697831
Tim S <stahta01@users.sourceforge.net>
2018-07-18 16:21:11 -0400

    Add msys64_7zip-3.0.4-shared_monolib.sh

---
+++ msys64_7zip-3.0.4-shared_monolib.sh
@@ -0,0 +1,21 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+# import local settings file
+source local_settings.sh
+
+# Change to git folder
+cd $WXWIDGETS_3_0_REPO || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout $WXWIDGETS_3_0_LFS_BRANCH || exit 2
+# git status -uno
+
+
+mkdir -p wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev/lib
+cp --recursive --update --preserve lib/gcc730MSYS2_x64_dllmono wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev/lib/
+
+7za u wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev.7z wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev/
+
+# echo "Finished"
