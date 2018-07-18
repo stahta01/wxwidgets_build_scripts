89dab4fa840e35a62ca6a3edf1e027d8402fab3e
Tim S <stahta01@users.sourceforge.net>
2018-07-18 16:20:21 -0400

    Add 7zip-3.0.4-headers.sh

---
+++ 7zip-3.0.4-headers.sh
@@ -0,0 +1,22 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+# import local settings file
+source local_settings.sh
+
+# Change to git folder
+cd $WXWIDGETS_3_0_REPO || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout $WXWIDGETS_3_0_LFS_BRANCH || exit 2
+# git status -uno
+
+
+mkdir -p wxWidgets-3.0.4_headers
+cp --recursive --update --preserve include wxWidgets-3.0.4_headers/
+rm -f wxWidgets-3.0.4_headers/include/wx/msw/setup.h
+
+7za u wxWidgets-3.0.4_headers.7z wxWidgets-3.0.4_headers\
+
+# echo "Finished"
