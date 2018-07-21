ba38d1882f095fd1bd66d8b02f0b6486f2e2b981
Tim S <stahta01@users.sourceforge.net>
2018-07-21 18:33:36 -0400

    ** Edit: local_settings.sh

--- local_settings.sh
+++ local_settings.sh
@@ -6,27 +6,34 @@ COMPILER_640TDM_BASE="/C/GreenApps32/mingw32-gcc-6.4.0-2"
 COMPILER_510TDM_BASE="/C/GreenApps32/MinGW_cb17.12"
 COMPILER_510TDM_x64_BASE="/C/Apps64/TDM-GCC"
 COMPILER_492TDM_BASE="/C/GreenApps32/MinGW_cb16.01"
-COMPILER_810W64_BASE="/C/Apps32/mingw-w64/i686-8.1.0-win32-sjlj-rt_v6-rev0/mingw32"
 MINGW_ORG_COMPILER_BASE="/C/Apps32/mingw.org"
 MINGW_ORG_COMPILER_VERSION="630M32"
 ZINJAI_COMPILER_BASE="/C/GreenApps32/mingw32-gcc6"
 ZINJAI_COMPILER_VERSION="630M32"
+MINGW64_POSIX_BASE="/C/Apps64/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64"
+MINGW64_POSIX_VERSION="810POSIX"
 
 
-# MSYS2_BASE and MSYS2_X64_BASE
+# MSYS2_BASE
 #    can not have a colon ":" in it.
-MSYS2_BASE="/C/msys64"
-MSYS2_X64_BASE="/C/msys64"
-MSYS2_COMPILER_BASE="$MSYS2_BASE/mingw32"
-MSYS2_COMPILER_X64_BASE="$MSYS2_X64_BASE/mingw64"
-MSYS2_COMPILER_VERSION="730MSYS2"
+if [[ $PROCESSOR_ARCHITECTURE == "x86" ]]; then
+  MSYS2_BASE="/C/msys32"
+else
+  MSYS2_BASE="/C/msys64"
+fi
+MSYS2_COMPILER_X64_BASE="$MSYS2_BASE/mingw64"
+MSYS2_COMPILER_X32_BASE="$MSYS2_BASE/mingw32"
+MSYS2_COMPILER_VERSION="930MSYS2"
 MSYS2_CLANG_VERSION="501CLANG"
 
-WXWIDGETS_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets"
+WXWIDGETS_REPO="$HOMEDRIVE/repos/git/wxWidgets"
 
-WXWIDGETS_3_0_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets_3_0_branch"
+WXWIDGETS_3_0_REPO="$HOMEDRIVE/repos/git/wxWidgets_3_0_branch"
 WXWIDGETS_3_0_TEST_BRANCH="testing3.0"
 WXWIDGETS_3_0_LFS_BRANCH="wxwidgets3.0.4_git-lfs"
 
-WXWIDGETS_2_8_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets_2_8_branch"
+WXWIDGETS_2_8_REPO="$HOMEDRIVE/repos/git/wxWidgets_2_8_branch"
 WXWIDGETS_2_8_LFS_BRANCH="wxwidgets2.8.12_git-lfs"
+
+WXWIDGETS_3_1_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets"
+WXWIDGETS_3_1_LFS_BRANCH="wxwidgets3.1.2_git-lfs"
