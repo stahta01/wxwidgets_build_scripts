0cbc217a2f628851badac0792e40827fe459e4c2
Tim S <stahta01@users.sourceforge.net>
2018-07-22 20:06:06 -0400

    Add msys32_7zip-3.0.4-shared_monolib.sh

--- msys64_7zip-3.0.4-shared_monolib.sh
+++ msys64_7zip-3.0.4-shared_monolib.sh
@@ -13,9 +13,9 @@ git checkout $WXWIDGETS_3_0_LFS_BRANCH || exit 2
 # git status -uno
 
 
-mkdir -p wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev/lib
-cp --recursive --update --preserve lib/gcc730MSYS2_x64_dllmono wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev/lib/
+mkdir -p wxMSW-3.0.4_gcc${MSYS2_COMPILER_VERSION}_x64_dllmono_Dev/lib
+cp --recursive --update --preserve lib/gcc${MSYS2_COMPILER_VERSION}_x64_dllmono wxMSW-3.0.4_gcc${MSYS2_COMPILER_VERSION}_x64_dllmono_Dev/lib/
 
-7za u wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev.7z wxMSW-3.0.4_gcc730MSYS2_x64_dllmono_Dev/
+7za u wxMSW-3.0.4_gcc${MSYS2_COMPILER_VERSION}_x64_dllmono_Dev.7z wxMSW-3.0.4_gcc${MSYS2_COMPILER_VERSION}_x64_dllmono_Dev/
 
 # echo "Finished"
