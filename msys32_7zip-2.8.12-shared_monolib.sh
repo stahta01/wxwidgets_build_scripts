38d9e10dec8887d217e4e2a59203e234fef50784
Tim S <stahta01@users.sourceforge.net>
2018-09-14 08:36:02 -0400

    Add msys32_7zip-2.8.12-shared_monolib.sh

---
+++ msys32_7zip-2.8.12-shared_monolib.sh
@@ -0,0 +1,21 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+# import local settings file
+source local_settings.sh
+
+# Change to git folder
+cd $WXWIDGETS_2_8_REPO || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout $WXWIDGETS_2_8_LFS_BRANCH || exit 2
+# git status -uno
+
+
+mkdir -p wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev/lib
+cp --recursive --update --preserve lib/gcc${MSYS2_COMPILER_X32_VERSION}_dllmono wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev/lib/
+
+7za u wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev.7z wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev/
+
+# echo "Finished"
