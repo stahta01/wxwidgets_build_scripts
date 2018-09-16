bf53779a25aa4392fc3cdca291d85da0ce7525e9
Tim S <stahta01@users.sourceforge.net>
2018-09-16 17:42:49 -0400

    Add: msys64_mingw32-make-3.x-static_libs.sh

---
+++ msys64_mingw32-make-3.x-static_libs.sh
@@ -0,0 +1,41 @@
+# This file is designed to be ran from (git for Windows) bash shell
+
+# import local settings file
+source local_settings.sh
+
+cd $WXWIDGETS_REPO || exit 1
+# pwd
+
+# Change to correct wxWidgets Git branch
+# git lfs install
+git checkout master || exit 2
+git pull
+git submodule init
+git submodule update
+# git status -uno
+
+export PATH=$MSYS2_COMPILER_X64_BASE/bin:$PATH
+
+# echo $PATH
+
+_COMPILER_VERSION="${MSYS2_COMPILER_VERSION}_x64"
+
+# echo $_COMPILER_VERSION
+
+cd build/msw || exit 3
+
+mingw32-make -f makefile.gcc \
+  CFG= \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=0 SHARED=0 UNICODE=1 BUILD=release
+
+mingw32-make -f makefile.gcc \
+  CFG= \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=0 SHARED=0 UNICODE=1 BUILD=debug
+
+# echo "Finished"
