e795cc998a3453350de4c9e8c206269524ccad24
Tim Stahlhut <stahta01@gmail.com>
2019-06-14 20:30:44 -0400

    Edit msys32 scripts to use MSYS2_COMPILER_VERSION

--- msys32_7zip-2.8.12-shared_monolib.sh
+++ msys32_7zip-2.8.12-shared_monolib.sh
@@ -13,9 +13,9 @@ git checkout $WXWIDGETS_2_8_LFS_BRANCH || exit 2
 # git status -uno
 
 
-mkdir -p wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev/lib
-cp --recursive --update --preserve lib/gcc${MSYS2_COMPILER_X32_VERSION}_dllmono wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev/lib/
+mkdir -p wxMSW-2.8.12_gcc${MSYS2_COMPILER_VERSION}_dllmono_Dev/lib
+cp --recursive --update --preserve lib/gcc${MSYS2_COMPILER_VERSION}_dllmono wxMSW-2.8.12_gcc${MSYS2_COMPILER_VERSION}_dllmono_Dev/lib/
 
-7za u wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev.7z wxMSW-2.8.12_gcc${MSYS2_COMPILER_X32_VERSION}_dllmono_Dev/
+7za u wxMSW-2.8.12_gcc${MSYS2_COMPILER_VERSION}_dllmono_Dev.7z wxMSW-2.8.12_gcc${MSYS2_COMPILER_VERSION}_dllmono_Dev/
 
 # echo "Finished"
