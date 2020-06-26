f2faac2aaf2eb36bb302fd2d3646f2f1cb2f821f
Tim Stahlhut <stahta01@gmail.com>
2020-06-25 22:50:53 -0400

    Edit CBgcc810_mingw32-make-3.1.3-shared_monolib.sh

--- CBgcc810_mingw32-make-3.1.3-shared_monolib.sh
+++ CBgcc810_mingw32-make-3.1.3-shared_monolib.sh
@@ -22,7 +22,6 @@ export PATH=/C/GreenApps64/MinGW_cb20.03/bin:$PATH
 #mingw32-make -f makefile.gcc \
   #CFG=mono \
   #COMPILER_VERSION=810_x64 \
-  #CPPFLAGS+="-D__MINGW64_TOOLCHAIN__" \
   #CXXFLAGS+="-fno-keep-inline-dllexport -std=gnu++11" \
   #MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release clean
 #rm -f $WXWIDGETS_REPO/include/wx/msw/setup.h
@@ -31,15 +30,19 @@ cd $WXWIDGETS_REPO/build/msw || exit 3
 mingw32-make -f makefile.gcc \
   CFG=mono \
   COMPILER_VERSION=810_x64 \
-  CPPFLAGS+="-D__MINGW64_TOOLCHAIN__" \
   CXXFLAGS+="-fno-keep-inline-dllexport -std=gnu++11" \
   MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release
+
+mingw32-make -f makefile.gcc \
+  CFG=mono \
+  COMPILER_VERSION=810_x64 \
+  CXXFLAGS+="-fno-keep-inline-dllexport -std=gnu++11" \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=debug
 
 cd $WXWIDGETS_REPO/samples/minimal || exit 4
 mingw32-make -f makefile.gcc \
   CFG=mono \
   COMPILER_VERSION=810_x64 \
-  CPPFLAGS+="-D__MINGW64_TOOLCHAIN__" \
   CXXFLAGS+="-fno-keep-inline-dllexport -std=gnu++11" \
   MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release
 
@@ -47,7 +50,6 @@ cd $WXWIDGETS_REPO/samples/drawing || exit 4
 mingw32-make -f makefile.gcc \
   CFG=mono \
   COMPILER_VERSION=810_x64 \
-  CPPFLAGS+="-D__MINGW64_TOOLCHAIN__" \
   CXXFLAGS+="-fno-keep-inline-dllexport -std=gnu++11" \
   MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release
 
@@ -55,7 +57,6 @@ cd $WXWIDGETS_REPO/tests || exit 5
 mingw32-make -f makefile.gcc \
   CFG=mono \
   COMPILER_VERSION=810_x64 \
-  CPPFLAGS+="-D__MINGW64_TOOLCHAIN__" \
   CXXFLAGS+="-fno-keep-inline-dllexport -std=gnu++11" \
   MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release
 
