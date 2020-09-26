6701dce18d987504628733c0c756f5234e07e4c0
Tim Stahlhut <stahta01@gmail.com>
2020-09-26 19:01:50 -0400

    Edit msys64_mingw32-make-3.x-shared_libs.sh comment out cleanup

--- msys64_mingw32-make-3.x-shared_libs.sh
+++ msys64_mingw32-make-3.x-shared_libs.sh
@@ -22,26 +22,26 @@ _COMPILER_VERSION="${MSYS2_COMPILER_VERSION}_x64"
 
 # echo $_COMPILER_VERSION
 
-####
-# Cleanup start
-cd $WXWIDGETS_REPO/build/msw || exit 3
-mingw32-make -f makefile.gcc \
-  CFG= \
-  VENDOR=$_COMPILER_VERSION \
-  COMPILER_VERSION=$_COMPILER_VERSION \
-  CXXFLAGS=-std=gnu++11 \
-  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=release clean
-mingw32-make -f makefile.gcc \
-  CFG= \
-  VENDOR=$_COMPILER_VERSION \
-  COMPILER_VERSION=$_COMPILER_VERSION \
-  CXXFLAGS=-std=gnu++11 \
-  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=debug  clean
-rm -f $WXWIDGETS_REPO/include/wx/msw/setup.h
-# Cleanup Finish
-####
+#####
+## Cleanup start
+#cd $WXWIDGETS_REPO/build/msw || exit 3
+#mingw32-make -f makefile.gcc \
+#  CFG= \
+#  VENDOR=$_COMPILER_VERSION \
+#  COMPILER_VERSION=$_COMPILER_VERSION \
+#  CXXFLAGS=-std=gnu++11 \
+#  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=release clean
+#mingw32-make -f makefile.gcc \
+#  CFG= \
+#  VENDOR=$_COMPILER_VERSION \
+#  COMPILER_VERSION=$_COMPILER_VERSION \
+#  CXXFLAGS=-std=gnu++11 \
+#  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=debug  clean
+#rm -f $WXWIDGETS_REPO/include/wx/msw/setup.h
+## Cleanup Finish
+#####
 
-cd build/msw || exit 3
+cd $WXWIDGETS_REPO/build/msw || exit 3
 
 mingw32-make -f makefile.gcc \
   CFG= \
