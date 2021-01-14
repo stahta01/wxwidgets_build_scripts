4462706db1d888874db55cf1b58c30c224be0181
Tim Stahlhut <stahta01@gmail.com>
2021-01-13 22:11:40 -0500

    Edit msys32_mingw32-make-3.x-shared_monolib.sh

--- msys32_mingw32-make-3.x-shared_monolib.sh
+++ msys32_mingw32-make-3.x-shared_monolib.sh
@@ -8,22 +8,48 @@ cd $WXWIDGETS_REPO || exit 1
 
 # Change to correct wxWidgets Git branch
 # git lfs install
+git restore include/wx/msw/setup0.h
 git checkout master || exit 2
 git pull
 git submodule init
 git submodule update
 # git status -uno
 
-export PATH=$MSYS2_COMPILER_BASE/bin:$PATH
+export PATH=$MSYS2_COMPILER_X32_BASE/bin:$PATH
 
 # echo $PATH
+# which mingw32-make
 
 _COMPILER_VERSION="${MSYS2_COMPILER_VERSION}"
 
 # echo $_COMPILER_VERSION
+# gcc --version
 
-cd build/msw || exit 3
+####
+# Cleanup start
+cd $WXWIDGETS_REPO/build/msw || exit 3
+mingw32-make -f makefile.gcc \
+  CFG=mono \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=release clean
+mingw32-make -f makefile.gcc \
+  CFG=mono \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=1 SHARED=1 UNICODE=1 BUILD=debug  clean
+#
+cp -f $WXWIDGETS_REPO/include/wx/msw/setup0.h $WXWIDGETS_REPO/include/wx/msw/setup.h
+# Cleanup Finish
+####
 
+sed --in-place "s/#define wxUSE_GRAPHICS_DIRECT2D 0/#define wxUSE_GRAPHICS_DIRECT2D wxUSE_GRAPHICS_CONTEXT/" $WXWIDGETS_REPO/include/wx/msw/setup.h
+
+## exit
+
+cd $WXWIDGETS_REPO/build/msw || exit 3
 mingw32-make -f makefile.gcc \
   CFG=mono \
   VENDOR=$_COMPILER_VERSION \
