afa52528581108a4ff590952261ff8672d766e90
Tim Stahlhut <stahta01@gmail.com>
2021-05-21 08:32:11 -0400

    Edit msys64_mingw32-make-3.x-shared_libs.sh

--- msys64_mingw32-make-3.x-shared_libs.sh
+++ msys64_mingw32-make-3.x-shared_libs.sh
@@ -8,6 +8,7 @@ cd $WXWIDGETS_REPO || exit 1
 
 # Change to correct wxWidgets Git branch
 # git lfs install
+git restore include/wx/msw/setup.h
 git checkout master || exit 2
 git pull
 git submodule init
@@ -22,38 +23,43 @@ _COMPILER_VERSION="${MSYS2_COMPILER_VERSION}_x64"
 
 # echo $_COMPILER_VERSION
 
-#####
-## Cleanup start
-#cd $WXWIDGETS_REPO/build/msw || exit 3
-#mingw32-make -f makefile.gcc \
-#  CFG= \
-#  VENDOR=$_COMPILER_VERSION \
-#  COMPILER_VERSION=$_COMPILER_VERSION \
-#  CXXFLAGS=-std=gnu++11 \
-#  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=release clean
-#mingw32-make -f makefile.gcc \
-#  CFG= \
-#  VENDOR=$_COMPILER_VERSION \
-#  COMPILER_VERSION=$_COMPILER_VERSION \
-#  CXXFLAGS=-std=gnu++11 \
-#  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=debug  clean
-#rm -f $WXWIDGETS_REPO/include/wx/msw/setup.h
-## Cleanup Finish
-#####
-
+####
+# Cleanup start
 cd $WXWIDGETS_REPO/build/msw || exit 3
-
 mingw32-make -f makefile.gcc \
-  CFG= \
   VENDOR=$_COMPILER_VERSION \
   COMPILER_VERSION=$_COMPILER_VERSION \
+  CPPFLAGS="-D__MINGW64_TOOLCHAIN__" \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=release clean
+mingw32-make -f makefile.gcc \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CPPFLAGS="-D__MINGW64_TOOLCHAIN__" \
+  CXXFLAGS=-std=gnu++11 \
+  MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=debug  clean
+#
+# cp -f $WXWIDGETS_REPO/include/wx/msw/setup0.h $WXWIDGETS_REPO/include/wx/msw/setup.h
+# Cleanup Finish
+####
+
+sed --in-place "s/#define wxUSE_GRAPHICS_DIRECT2D 0/#define wxUSE_GRAPHICS_DIRECT2D 1/" $WXWIDGETS_REPO/include/wx/msw/setup.h
+sed --in-place "s/#define wxUSE_GRAPHICS_CONTEXT 0/#define wxUSE_GRAPHICS_CONTEXT 1/" $WXWIDGETS_REPO/include/wx/msw/setup.h
+
+## exit
+
+cd $WXWIDGETS_REPO/build/msw || exit 3
+mingw32-make -f makefile.gcc \
+  VENDOR=$_COMPILER_VERSION \
+  COMPILER_VERSION=$_COMPILER_VERSION \
+  CPPFLAGS="-D__MINGW64_TOOLCHAIN__" \
   CXXFLAGS=-std=gnu++11 \
   MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=release
 
 mingw32-make -f makefile.gcc \
-  CFG= \
   VENDOR=$_COMPILER_VERSION \
   COMPILER_VERSION=$_COMPILER_VERSION \
+  CPPFLAGS="-D__MINGW64_TOOLCHAIN__" \
   CXXFLAGS=-std=gnu++11 \
   MONOLITHIC=0 SHARED=1 UNICODE=1 BUILD=debug
 
