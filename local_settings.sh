6d29402e14ed2485c9bc3e4495cb6e103f42e5f9
Tim Stahlhut <stahta01@gmail.com>
2022-05-18 08:18:04 -0400

    Edit local_settings.sh to use H USB drive locations

--- local_settings.sh
+++ local_settings.sh
@@ -8,23 +8,25 @@ MINGW_ORG_COMPILER_VERSION="920M32"
 # MSYS2_BASE
 #    can not have a colon ":" in it.
 if [[ $PROCESSOR_ARCHITECTURE == "x86" ]]; then
-  MSYS2_BASE="/C/msys32"
+  MSYS2_BASE="/H/msys32"
 else
-  MSYS2_BASE="/C/msys64"
+  MSYS2_BASE="/H/msys64"
 fi
 MSYS2_COMPILER_X64_BASE="$MSYS2_BASE/mingw64"
 MSYS2_COMPILER_X32_BASE="$MSYS2_BASE/mingw32"
-MSYS2_COMPILER_VERSION="1030MSYS2"
+MSYS2_COMPILER_VERSION="1130MSYS2"
 MSYS2_CLANG_VERSION="501CLANG"
 
-WXWIDGETS_REPO="$HOMEDRIVE/repos/git/wxWidgets"
+WXWIDGETS_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets"
 
-WXWIDGETS_3_0_REPO="$HOMEDRIVE/repos/git/wxWidgets_3_0_branch"
+WXWIDGETS_3_0_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets_3_0_branch"
 WXWIDGETS_3_0_TEST_BRANCH="testing3.0"
-WXWIDGETS_3_0_LFS_BRANCH="wxwidgets3.0.4_git-lfs"
+WXWIDGETS_3_0_PROD_BRANCH="wxwidgets3.0.5_git"
 
-WXWIDGETS_2_8_REPO="$HOMEDRIVE/repos/git/wxWidgets_2_8_branch"
-WXWIDGETS_2_8_LFS_BRANCH="wxwidgets2.8.12_git-lfs"
+WXWIDGETS_2_8_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets_2_8_branch"
+WXWIDGETS_2_8_PROD_BRANCH="wxwidgets2.8.12_git"
 
-WXWIDGETS_3_1_REPO="$HOMEDRIVE/Users/$USERNAME/devel/open_source_code/version_control/git_repos/wxWidgets"
-WXWIDGETS_3_1_LFS_BRANCH="wxwidgets3.1.2_git-lfs"
+WXWIDGETS_3_1_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets"
+WXWIDGETS_3_1_PROD_BRANCH="wxwidgets3.1.6_git"
+
+
