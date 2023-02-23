7e486dee8f4407b1f4a87291cfa52af597b0a042
Tim Stahlhut <stahta01@gmail.com>
2023-02-22 20:45:19 -0500

    Edit local_settings.sh

--- local_settings.sh
+++ local_settings.sh
@@ -14,20 +14,17 @@ else
 fi
 MSYS2_COMPILER_X64_BASE="$MSYS2_BASE/mingw64"
 MSYS2_COMPILER_X32_BASE="$MSYS2_BASE/mingw32"
-MSYS2_COMPILER_VERSION="1130"
-MSYS2_CLANG_VERSION="501CLANG"
+MSYS2_COMPILER_VERSION="1220"
+MSYS2_CLANG_VERSION="1507CLANG"
 
 WXWIDGETS_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets"
 
-WXWIDGETS_3_0_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets_3_0_branch"
+WXWIDGETS_3_0_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets_3_0_branch"
 WXWIDGETS_3_0_TEST_BRANCH="testing3.0"
 WXWIDGETS_3_0_PROD_BRANCH="wxwidgets3.0.5_git"
 
-WXWIDGETS_2_8_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets_2_8_branch"
+WXWIDGETS_2_8_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets_2_8_branch"
 WXWIDGETS_2_8_PROD_BRANCH="wxwidgets2.8.12_git"
 
-WXWIDGETS_3_1_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets"
-WXWIDGETS_3_1_PROD_BRANCH="wxwidgets3.1.6_git"
-
-WXWIDGETS_3_2_REPO="H:/repos/git/devel/wxwidgets_libs/wxWidgets_3_2_branch"
+WXWIDGETS_3_2_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets_3_2_branch"
 WXWIDGETS_3_2_PROD_BRANCH="wxwidgets3.2.0_git"
