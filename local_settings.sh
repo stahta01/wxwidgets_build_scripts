13edd4b5b89de59b113d56e8eb1b01d393c16df6
Tim Stahlhut <stahta01@gmail.com>
2025-09-28 14:46:33 -0400

    Edit local_settings.sh switch to C Drive and
    update MSys2 GCC compiler version

--- local_settings.sh
+++ local_settings.sh
@@ -8,29 +8,29 @@ MINGW_ORG_COMPILER_VERSION="920M32"
 # MSYS2_BASE
 #    can not have a colon ":" in it.
 if [[ $PROCESSOR_ARCHITECTURE == "x86" ]]; then
-  MSYS2_BASE="/H/msys32"
+  MSYS2_BASE="/C/msys32"
 else
-  MSYS2_BASE="/H/msys64"
+  MSYS2_BASE="/C/msys64"
 fi
-MSYS2_COMPILER_X64_BASE="$MSYS2_BASE/mingw64"
+MSYS2_COMPILER_X64_BASE="$MSYS2_BASE/ucrt64"
 MSYS2_COMPILER_X32_BASE="$MSYS2_BASE/mingw32"
-MSYS2_COMPILER_VERSION="1320"
+MSYS2_COMPILER_VERSION="1520"
 MSYS2_CLANG_VERSION="1507CLANG"
 
-CB_NIGHTLY_COMPILER_BASE=/H/GreenApps/winlibs-x86_64-posix-seh-gcc-14.1.0-mingw-w64ucrt-11.0.1-r1/mingw64
+CB_NIGHTLY_COMPILER_BASE=/C/GreenApps/winlibs-x86_64-posix-seh-gcc-14.1.0-mingw-w64ucrt-11.0.1-r1/mingw64
 CB_NIGHTLY_COMPILER_VERSION="14.1.0"
 
 CB_RELEASE_COMPILER_BASE=/C/PROGRA~1/CODEBL~1/MinGW
 CB_RELEASE_COMPILER_VERSION="8.1.0"
 
-WXWIDGETS_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets"
+WXWIDGETS_REPO="C:/devel/wxwidgets_libs/repos/wxWidgets"
 
-WXWIDGETS_3_0_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets_3_0_branch"
+WXWIDGETS_3_0_REPO="C:/devel/wxwidgets_libs/repos/wxWidgets_3_0_branch"
 WXWIDGETS_3_0_TEST_BRANCH="testing3.0"
 WXWIDGETS_3_0_PROD_BRANCH="wxwidgets3.0.5_git"
 
-WXWIDGETS_2_8_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets_2_8_branch"
+WXWIDGETS_2_8_REPO="C:/devel/wxwidgets_libs/repos/wxWidgets_2_8_branch"
 WXWIDGETS_2_8_PROD_BRANCH="wxwidgets2.8.12_git"
 
-WXWIDGETS_3_2_REPO="H:/devel/wxwidgets_libs/repos/wxWidgets_3_2_branch"
+WXWIDGETS_3_2_REPO="C:/devel/repos/wxwidgets_libs/wxWidgets_3_2_branch"
 WXWIDGETS_3_2_PROD_BRANCH="wxwidgets3.2.2_git"
